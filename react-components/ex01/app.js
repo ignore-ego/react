d25fa9edf08fe49633b27e227e7e3ed6c94e2547
George Ion <89georgeion@gmail.com>
2023-11-16 22:03:26 +0200

    react ex 1 -2

--- react-components/ex01/app.js
+++ react-components/ex01/app.js
@@ -31,3 +31,34 @@ const person = {
     },
   ],
 };
+
+console.warn(
+  `Folosind metoda map pe arrayul skills, returneaza si afiseaza in consola un array in care fiecare consoana este scrisa cu litera mare (vocalele nu) `,
+);
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const arrVowels = person.skills.map((skill) => {
+  const letters = skill.split('');
+  const upperCaseVowels = letters.map((letter) => {
+    if (vowels.includes(letter)) {
+      return letter.toLocaleUpperCase();
+    }
+
+    return letter;
+  });
+
+  return upperCaseVowels.join('');
+});
+
+console.log(arrVowels);
+
+console.warn(`Folosind map pe arrayul friends, returneaza un array in care fiecare pozitie contine propozitia
+“Ma numesc {name} {surname} si am {age} ani.”`);
+
+const sentences = person.friends.map((friend) => {
+  const { name, surname, age } = friend;
+
+  return `Ma numesc ${name} ${surname} si am ${age} ani.`;
+});
+
+console.log(sentences);
