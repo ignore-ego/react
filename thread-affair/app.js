1205fa5bcaf5db0f9802e956f59d84b563813282
George Ion <89georgeion@gmail.com>
2023-12-14 22:02:38 +0200

    wishlist si newsletter

--- thread-affair/app.js
+++ thread-affair/app.js
@@ -1,3 +1,8 @@
+const ADD_TO_CART_EVENT = 'cart/productAdded';
+const REMOVE_FROM_CART_EVENT = 'cart/productRemoved';
+const ADD_TO_WISHLIST_EVENT = 'wl/productAdded';
+const REMOVE_FROM_WISHLIST_EVENT = 'wl/productRemoved';
+
 const AddToCartButton = ({ productId }) => {
   const state = React.useState({
     added: false,
@@ -16,7 +21,8 @@ const AddToCartButton = ({ productId }) => {
 
     // simulate request
     setTimeout(() => {
-      const eventName = added === true ? 'removeFromCart' : 'addToCart';
+      const eventName =
+        added === true ? REMOVE_FROM_CART_EVENT : ADD_TO_CART_EVENT;
 
       dispatchEvent(
         new CustomEvent(eventName, {
@@ -60,7 +66,8 @@ const AddToWishlistButton = ({ productId }) => {
     });
 
     setTimeout(() => {
-      const eventName = added === true ? 'removeFromWl' : 'addToWl';
+      const eventName =
+        added === true ? REMOVE_FROM_WISHLIST_EVENT : ADD_TO_WISHLIST_EVENT;
 
       dispatchEvent(
         new CustomEvent(eventName, {
@@ -95,8 +102,8 @@ const ProductControls = (props) => {
   const { productId } = props;
 
   return [
-    <AddToCartButton productId={productId}></AddToCartButton>,
-    <AddToWishlistButton productId={productId}></AddToWishlistButton>,
+    <AddToCartButton productId={productId} key={0}></AddToCartButton>,
+    <AddToWishlistButton productId={productId} key={1}></AddToWishlistButton>,
   ];
 };
 
@@ -116,20 +123,26 @@ const HeaderCartCounter = () => {
   const setState = state[1];
 
   React.useEffect(() => {
-    addEventListener('addToCart', ({ detail }) => {
+    const handler = ({ detail }) => {
       const { productId } = detail;
-
+      alert('casualy pops alert');
       setState((previousState) => {
         return {
           productIds: [...previousState.productIds, productId],
           qty: previousState.qty + 1,
         };
       });
-    });
+    };
+
+    addEventListener(ADD_TO_CART_EVENT, handler);
+
+    return () => {
+      removeEventListener(ADD_TO_CART_EVENT, handler);
+    };
   }, []);
 
   React.useEffect(() => {
-    addEventListener('removeFromCart', ({ detail }) => {
+    const handler = ({ detail }) => {
       setState((previousState) => {
         return {
           productIds: previousState.productIds.filter((productId) => {
@@ -138,7 +151,13 @@ const HeaderCartCounter = () => {
           qty: previousState.qty - 1,
         };
       });
-    });
+    };
+
+    addEventListener(REMOVE_FROM_CART_EVENT, handler);
+
+    return () => {
+      removeEventListener(REMOVE_FROM_CART_EVENT, handler);
+    };
   }, []);
 
   const showProducts = () => {
@@ -153,7 +172,7 @@ const HeaderCartCounter = () => {
   };
 
   return (
-    <div className="header-cart" onClick={showProducts}>
+    <div className="header-counter" onClick={showProducts}>
       <span className="cart-qty">{actualState.qty}</span>
       <i className="fas fa-shopping-cart icon"></i>
     </div>
@@ -161,32 +180,178 @@ const HeaderCartCounter = () => {
 };
 
 const HeaderWlCounter = () => {
+  // nested destructure
+  const [{ productIds, qty }, setState] = React.useState({
+    productIds: [],
+    qty: 0,
+  });
+
   React.useEffect(() => {
-    addEventListener('addToWl', ({ detail }) => {
-      alert(detail.productId);
-    });
+    const handler = ({ detail }) => {
+      const { productId } = detail;
+
+      setState(({ productIds, qty }) => {
+        return {
+          productIds: [...productIds, productId],
+          qty: ++qty,
+        };
+      });
+    };
+    addEventListener(ADD_TO_WISHLIST_EVENT, handler);
+
+    return () => {
+      removeEventListener(ADD_TO_WISHLIST_EVENT, handler);
+    };
   }, []);
 
   React.useEffect(() => {
-    addEventListener('removeFromWl', ({ detail }) => {});
+    const handler = ({ detail }) => {
+      setState(({ productIds, qty }) => {
+        return {
+          productIds: productIds.filter((productId) => {
+            return productId !== detail.productId;
+          }),
+          qty: --qty,
+        };
+      });
+    };
+    addEventListener(REMOVE_FROM_WISHLIST_EVENT, handler);
+
+    return () => {
+      removeEventListener(REMOVE_FROM_WISHLIST_EVENT, handler);
+    };
   }, []);
+
+  const showProducts = () => {
+    const message =
+      qty <= 0
+        ? 'There are no product in your wishlist.'
+        : `There are the pids in your wishlist: ${productIds}`;
+
+    alert(message);
+  };
 
   return (
-    <div className="header-cart">
-      <span className="cart-qty">ceva</span>
+    <div className="header-counter" onClick={showProducts}>
+      <span className="cart-qty">{qty}</span>
       <i className="fas fa-heart icon"></i>
     </div>
   );
 };
 
 const HeaderCounters = () => {
+  const [showButtons, setsShowButtons] = React.useState(true);
+  const toggleCounters = () => {
+    setsShowButtons(!showButtons);
+  };
+
   return (
     <>
-      <HeaderCartCounter></HeaderCartCounter>
-      <HeaderWlCounter></HeaderWlCounter>
+      <button title="Toggle" type="button" onClick={toggleCounters}>
+        Toggle
+      </button>
+
+      {showButtons ? (
+        <>
+          <HeaderCartCounter></HeaderCartCounter>
+          <HeaderWlCounter></HeaderWlCounter>
+        </>
+      ) : null}
     </>
   );
 };
 
 const headerCounters = document.querySelector('.header-counters');
 ReactDOM.createRoot(headerCounters).render(<HeaderCounters></HeaderCounters>);
+
+const validateEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(email);
+};
+
+const NewsletterForm = () => {
+  const [state, setState] = React.useState({
+    email: '',
+    formMessage: '',
+    busy: false,
+    successMessage: '',
+  });
+  const { email, formMessage, busy, successMessage } = state;
+
+  // controlled input
+  const onChange = (event) => {
+    // just target recommended
+    setState({
+      ...state,
+      email: event.target.value,
+      formMessage: '',
+    });
+  };
+
+  const send = (event) => {
+    event.preventDefault();
+    // event.target['field-newsletter'].value
+
+    if (busy) {
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setState({
+        ...state,
+        formMessage: 'Please use a valid email',
+      });
+
+      return;
+    }
+
+    setState({
+      ...state,
+      busy: true,
+      formMessage: '',
+    });
+
+    // simulate ajax
+    setTimeout(() => {
+      setState({
+        ...state,
+        busy: false,
+        email: '',
+        successMessage: `Emailul ${email} a fost inscris.`,
+      });
+    }, 1200);
+  };
+
+  if (successMessage.length > 0) {
+    return <div className="container">{successMessage}</div>;
+  }
+
+  return (
+    <form className="form-newsletter container" onSubmit={send}>
+      <label htmlFor="field-newsletter">
+        Subscribe to our <span>newsletter</span>
+      </label>
+
+      <input
+        type="text"
+        name="field-newsletter"
+        id="field-newsletter"
+        placeholder="enter your email address to receive the latest news!"
+        value={email}
+        onChange={onChange}
+      ></input>
+
+      <button title="Subscribe" type="submit" disabled={busy}>
+        {busy ? '...loading' : 'Subscribe'}
+      </button>
+
+      <div className="form-message">{formMessage}</div>
+    </form>
+  );
+};
+
+ReactDOM.createRoot(document.querySelector('section.home-newsletter')).render(
+  <NewsletterForm></NewsletterForm>,
+);
